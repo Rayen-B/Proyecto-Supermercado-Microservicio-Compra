605d4d626397a1de02988f179e41a864859f9d41
isidora <is.gomezj@duocuc.cl>
2026-06-25 15:51:03 -0400

    Traslado del manejo de errores, la configuración de Kafka y OpenAPI, ademas del controller de compra

--- Compra/src/main/java/cl/supermercado/compra/controller/CompraController.java
+++ Compra/src/main/java/cl/supermercado/compra/controller/CompraController.java
@@ -1,6 +1,10 @@
 package cl.supermercado.compra.controller;
+import cl.supermercado.compra.dto.request.CompraRequestDto;
+import cl.supermercado.compra.dto.response.CompraResponseDto;
 import cl.supermercado.compra.model.Compra;
 import cl.supermercado.compra.service.CompraService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
@@ -10,23 +14,24 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/compras")
+@RequiredArgsConstructor
 public class CompraController {
-    @Autowired
-    private CompraService compraService;
+
+    private final CompraService compraService;
 
     @PostMapping
-    public ResponseEntity<CompraDTO> crear(@RequestBody Compra compra) {
-        Compra nueva = compraService.crear(compra);
-        CompraDTO dto = new CompraDTO(nueva.getId(), nueva.getUsuarioId(), nueva.getTotal(), nueva.getFinalizada());
-        return ResponseEntity.ok(dto);
+    public ResponseEntity<CompraResponseDto> crearCompra(@Valid @RequestBody CompraRequestDto request) {
+        return ResponseEntity.ok(compraService.crearCompra(request));
     }
 
     @GetMapping
-    public ResponseEntity<List<CompraDTO>> listar() {
-        List<CompraDTO> compras = compraService.listar()
-                .stream()
-                .map(c -> new CompraDTO(c.getId(), c.getUsuarioId(), c.getTotal(), c.getFinalizada()))
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(compras);
+    public ResponseEntity<List<CompraResponseDto>> listarCompras() {
+        return ResponseEntity.ok(compraService.listarCompras());
     }
+
+    @GetMapping("/usuario/{usuarioId}")
+    public ResponseEntity<List<CompraResponseDto>> listarComprasPorUsuario(@PathVariable Long usuarioId) {
+        return ResponseEntity.ok(compraService.listarComprasPorUsuario(usuarioId));
+    }
+
 }
